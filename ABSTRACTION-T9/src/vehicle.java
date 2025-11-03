13a7a8add381812d1982c1974439394e387e1582
Isingoma <klaus.gma2@gmail.com>
2025-11-03 19:54:40 +0300

    Commit 2
    In the new version, on lines 3 and 9, I corrected a spelling error of brand, and on lines 15 to 19, I added a concrete method to compute rental cost based on days.

--- ABSTRACTION-T9/src/vehicle.java
+++ ABSTRACTION-T9/src/vehicle.java
@@ -1,15 +1,21 @@
 abstract class Vehicle {
 
-    protected String brnad;
+    protected String brand;
     protected String model;
     protected int year;
     protected double basePrice;
 
     public Vehicle(String brand, String model, int year, double basePrice) {
-        this.brnad = brand;
+        this.brand = brand;
         this.model = model;
         this.year = year;
         this.basePrice = basePrice;
     }
     public abstract void displayVehicleInfo();
+
+    public double computeRentalCost(int days) {
+        double dailyRate = basePrice * 0.05;
+        return dailyRate * days;
+    }
 }
+
