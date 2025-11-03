967bfceb9f7f7c472cba5451d3ddf943adcc7dba
Isingoma <klaus.gma2@gmail.com>
2025-11-03 21:05:51 +0300

    Commit 1
    I created a subclass, Truck, and it inherits from the base class Vehicle with 2 instance variables, a constructor and a method that was overridden from the base class to display the vehicle information.

---
+++ ABSTRACTION-T9/src/Truck.java
@@ -0,0 +1,23 @@
+class Truck extends Vehicle {
+
+    private double loadCapacity;
+    private int numberOfAxles;
+
+    public Truck(String brand, String model, int year, double basePrice, double loadCapacity, int numberOfAxles) {
+        super(brand, model, year, basePrice);
+        this.loadCapacity = loadCapacity;
+        this.numberOfAxles = numberOfAxles;
+    }
+    @Override
+    public void displayVehicleInfo() {
+        System.out.println("\n___Truck Information___");
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Base Price: $" + basePrice);
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+        System.out.println("Number of Axles: " + numberOfAxles);
+    }
+
+
+}
