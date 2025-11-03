de7e7bb03c841412536e90abeb4d6c087e34a551
Isingoma <klaus.gma2@gmail.com>
2025-11-03 20:24:46 +0300

    Commit 2
    In the new version, on line 1, I extended the Car subclass to the Vehicle base class, on line 7 I added the super keyword to refer to the base class objects, on lines 11 to 19 i added a display method overriden from the base class to display the vehicle information and on lines 21 to 24i added a concrete method to compute the rental cost of the Car

--- ABSTRACTION-T9/src/Car.java
+++ ABSTRACTION-T9/src/Car.java
@@ -1,12 +1,26 @@
-public class Car {
+public class Car extends Vehicle {
 
     private int numberOfDoors;
     private String feultype;
 
     public Car(String brand, String model, int year, double basePrice, int numberOfDoors, String feultype) {
-
+        super(brand, model, year, basePrice);
         this.numberOfDoors = numberOfDoors;
         this.feultype = feultype;
+    }
+    @Override
+    public void displayVehicleInfo() {
+        System.out.println("\n___Car Information___");
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Base Price: $" + basePrice);
+        System.out.println("Number of Doors: " + numberOfDoors);
+        System.out.println("Fuel Type: " + feultype);
+    }
+    @Override
+    public double computeRentalCost(int days) {
+        return super.computeRentalCost(days) * 1.1;
     }
 }
 
