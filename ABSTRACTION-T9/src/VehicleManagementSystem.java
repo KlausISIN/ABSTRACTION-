de030ea3d8a3d3c92cbdc797aa26cb422043703c
Isingoma <klaus.gma2@gmail.com>
2025-11-03 22:08:46 +0300

    Commit 2
    In the new version on line 11 i added an array method to store the vehicles in an array format for easy iteration,  on line 13 i added a parameter for the number of days computation, on line 15 to 19 i added a display details and compute rental costs for each vehicle and on lines 22 to 35 i added an extra display to show how much it would cost to rent the selected vehicle for various days according to the customers needs and on line 38 to 39 i added a display message to thank the customer for using our vehicle management system

--- ABSTRACTION-T9/src/VehicleManagementSystem.java
+++ ABSTRACTION-T9/src/VehicleManagementSystem.java
@@ -8,8 +8,35 @@ public class VehicleManagementSystem {
         Motorcycle motorcycle = new Motorcycle("DUCATI", "175-T", 1958, 9700, 174, true);
         Truck truck = new Truck("MERCEDES BENZ", "E-ACTROS", 2025, 250000, 25, 3);
 
+        Vehicle[] vehicles = {car, motorcycle, truck};
+
+        int rentalDays = 5;
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.displayVehicleInfo();
+            double rentalCost = vehicle.computeRentalCost(rentalDays);
+            System.out.printf("Rental Cost for %d days: $%.2f%n", rentalDays, rentalCost);
+            System.out.println("--------------------------------------------------");
+
+        }
+        int[] variousdays = {1, 2, 15, 30};
+        System.out.println("\nRental costs for Car for various days:");
+        for (int days : variousdays) {
+            System.out.println(days + " day(s): $" + String.format("%.2f", car.computeRentalCost(days)));
+
+
+        }
+        System.out.println("\nRental costs for Motorcycle for various days:");
+        for (int days : variousdays) {
+            System.out.println(days + " day(s): $" + String.format("%.2f", motorcycle.computeRentalCost(days)));
+        }
+        System.out.println("\nRental costs for Truck for various days:");
+        for (int days : variousdays) {
+            System.out.println(days + " day(s): $" + String.format("%.2f", truck.computeRentalCost(days)));
+        }
+
+        System.out.println("\nThank you for using our Vehicle Management System!");
+        System.out.println("==================================================");
 
     }
-
 }
-
