ca8b7654b78e10c36e5fd94de417cf688c4e4f6d
Isingoma <klaus.gma2@gmail.com>
2025-11-03 20:12:43 +0300

    Commit 1
    I declared a class and named it Car, which will be a concrete subclass and created a constructor for it

---
+++ ABSTRACTION-T9/src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    private int numberOfDoors;
+    private String feultype;
+
+    public Car(String brand, String model, int year, double basePrice, int numberOfDoors, String feultype) {
+
+        this.numberOfDoors = numberOfDoors;
+        this.feultype = feultype;
+    }
+}
+
